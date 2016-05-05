68f952e2a41166c7e2cf12a53a55d309c05f3dea
Ivan Mykhailovskyi <ivan.mykhailovskyi@gmail.com>
2016-05-05 17:13:09 +0300

    Implemented error codes handling.

--- src/main/java/carshow/exceptions/resolvers/CustomExceptionResolver.java
+++ src/main/java/carshow/exceptions/resolvers/CustomExceptionResolver.java
@@ -20,7 +20,7 @@ public class CustomExceptionResolver extends SimpleMappingExceptionResolver {
 	private ModelAndView getCustomModelAndView(String viewName, Exception ex,
 			HttpServletRequest request){
 		ModelAndView mv = super.getModelAndView(viewName, ex);
-                mv.addObject("message", ex.getMessage());
+                mv.addObject("code", "internal.server.error");
                 ex.printStackTrace();
                 mv.addObject("exception_message", ex.toString());
 		return mv;
